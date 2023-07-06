7ea2ac1768830dfc356e78ef1f58ec60c49671e6
OsyTee <OsyTee@github.com>
2023-07-06 10:47:52 +0000

    Task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -36,3 +36,67 @@ type Subjects = 'Math' | 'History';
 export const teachClass = (todayClass: Subjects) : string => {
   return todayClass === 'Math' ? 'Teaching Math': 'Teaching History';
 }
+
+
+// Employee constructor function
+function Employee(salary) {
+  this.salary = salary;
+}
+
+// Director constructor function
+function Director(salary) {
+  Employee.call(this, salary);
+}
+
+// Teacher constructor function
+function Teacher(salary) {
+  Employee.call(this, salary);
+}
+
+// Function to create an employee object based on salary
+function createEmployee(salary) {
+  if (salary >= 1000) {
+    return new Director(salary);
+  } else {
+    return new Teacher(salary);
+  }
+}
+
+// Type predicate function to check if an employee is a director
+function isDirector(employee) {
+  return employee instanceof Director;
+}
+
+// Function to perform work based on employee type
+function executeWork(employee) {
+  if (isDirector(employee)) {
+    workDirectorTasks();
+  } else {
+    workTeacherTasks();
+  }
+}
+
+// Function to perform director tasks
+function workDirectorTasks() {
+  console.log('Getting to director tasks');
+}
+
+// Function to perform teacher tasks
+function workTeacherTasks() {
+  console.log('Getting to teacher tasks');
+}
+
+
+// String literal type
+type Subjects = 'Math' | 'History';
+
+// Function to teach a class based on the subject
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    return 'Invalid subject';
+  }
+}
